cc3388b1ab3bddb67c9ad1cccc8cf661f1697204
Ricardo Ariel Costa Souza <ricardo.devmob@gmail.com>
2023-07-18 18:11:57 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "github.com/RicardoArielSouza/gopportunies.git/router"
+import (
+	"github.com/RicardoArielSouza/gopportunies.git/config"
+	"github.com/RicardoArielSouza/gopportunies.git/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+	// Initialize Configs
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
+
+	// Initialize Router
 	router.Initialize()
 }
